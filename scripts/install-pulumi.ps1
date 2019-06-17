aa4aef6501ef8e6f6a8ca979f166f2891e1b09d9
Christian Eder <christian.eder@zuehlke.com>
2019-06-17 08:55:39 +0200

    add pulumi installation script

---
+++ scripts/install-pulumi.ps1
@@ -0,0 +1,2 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString('https://get.pulumi.com/install.ps1'))
+SET "PATH=%PATH%;%USERPROFILE%\.pulumi\bin"
