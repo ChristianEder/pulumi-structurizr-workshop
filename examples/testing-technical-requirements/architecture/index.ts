c1b163d2822896259e986894add364ccd279812c
Christian Eder <christian.eder@zuehlke.com>
2019-06-24 09:33:24 +0200

    add quality requirement test example

---
+++ examples/testing-technical-requirements/architecture/index.ts
@@ -0,0 +1,19 @@
+import * as infrastructure from '../infrastructure';
+import * as pulumi from "@pulumi/pulumi";
+import { describe, it } from "mocha";
+import { expect } from "chai";
+
+describe("infrastructure", () => {
+
+  describe("web app", () => {
+
+    it("should only support HTTPS requests", (done) => {
+      infrastructure.app.httpsOnly.apply(h => { 
+        expect(h).to.equal(true);
+        done();
+       });
+    });
+
+  });
+
+});
---
+++ examples/testing-technical-requirements/infrastructure/index.ts
@@ -0,0 +1,24 @@
+import * as azure from "@pulumi/azure"
+import * as pulumi from "@pulumi/pulumi";
+
+export const resourceGroup = new azure.core.ResourceGroup("resourceGroupName", {
+  location: azure.Locations.WestEurope,
+});
+
+export const plan = new azure.appservice.Plan("test-web-app", {
+  resourceGroupName: resourceGroup.name,
+  sku: {
+    tier: "Free",
+    size: "F1",
+  }
+});
+
+export const app = new azure.appservice.AppService("test-web-app", {
+  resourceGroupName: resourceGroup.name,
+  appServicePlanId: plan.id,
+  httpsOnly: true
+});
+
+export const url = pulumi.interpolate`https://${app.defaultSiteHostname}`;
+
+
