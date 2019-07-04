5c308fde2a4a127a593c45e8800eb48cdbb0850b
Christian Eder <christian.eder@zuehlke.com>
2019-07-04 10:38:55 +0200

    addtional slides

--- examples/azure-web-app/pulumi/index.ts
+++ examples/azure-web-app/pulumi/index.ts
@@ -20,3 +20,6 @@ const app = new azure.appservice.AppService("test-web-app", {
 
 export const url = pulumi.interpolate`https://${app.defaultSiteHostname}`;
 
+
+
+const resourceGroups = ["euwest", "uscentral"].map(location => new azure.core.ResourceGroup(`test-web-app-${location}`, { location }));
