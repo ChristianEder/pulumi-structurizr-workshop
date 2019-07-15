8034a12bfc49efafa050e742c32f50aa2291805b
ChristianEder <christian.johann.eder@googlemail.com>
2019-07-15 07:35:51 +0200

    Update install-pulumi.ps1

--- scripts/install-pulumi.ps1
+++ scripts/install-pulumi.ps1
@@ -1,2 +1,1 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString('https://get.pulumi.com/install.ps1'))
-SET "PATH=%PATH%;%USERPROFILE%\.pulumi\bin"
+[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";" + $Env:USERPROFILE + "\.pulumi\bin", [System.EnvironmentVariableTarget]::User)
