e1c37a85b4eda35e727963c2df8e51cd465b08d9
Christian Eder <christian.eder@zuehlke.com>
2019-10-14 17:20:59 +0200

    install extensions to correct user, add desktop link for VS Code

--- scripts/clone-and-install.ps1
+++ scripts/clone-and-install.ps1
@@ -4,4 +4,9 @@ cd C:\Users\dev\repos\pulumi-structurizr-workshop\scripts
 .\install-dependencies.ps1
 .\install-vs-code-extensions.ps1
 cd ..
-& 'C:\Program Files\Microsoft VS Code\bin\code' .
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut("C:\Users\dev\Desktop\Open Workshop Repository in VS Code.lnk")
+$Shortcut.TargetPath = "vscode://file/C:/Users/dev/repos/pulumi-structurizr-workshop"
+$Shortcut.IconLocation = "C:\Program Files\Microsoft VS Code\Code.exe"
+$Shortcut.Save()
--- scripts/install-vs-code-extensions.ps1
+++ scripts/install-vs-code-extensions.ps1
@@ -1,4 +1,4 @@
-& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension yzhang.markdown-all-in-one --force
-& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension jebbs.plantuml --force
-& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension mauve.terraform --force
-& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension ms-vscode.powershell --force
+& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension yzhang.markdown-all-in-one --force --extensions-dir 'C:\Users\dev\.vscode\extensions'
+& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension jebbs.plantuml --force --extensions-dir 'C:\Users\dev\.vscode\extensions'
+& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension mauve.terraform --force --extensions-dir 'C:\Users\dev\.vscode\extensions'
+& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension ms-vscode.powershell --force --extensions-dir 'C:\Users\dev\.vscode\extensions'
