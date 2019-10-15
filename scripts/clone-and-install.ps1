3f3a212857582eb508bd44218929f5977af1601d
Christian Eder <christian.eder@zuehlke.com>
2019-10-15 16:18:31 +0200

    hopefully now using the correct user dir

--- scripts/clone-and-install.ps1
+++ scripts/clone-and-install.ps1
@@ -1,12 +1,18 @@
-& 'C:\Program Files\Git\cmd\git' clone https://github.com/ChristianEder/pulumi-structurizr-workshop.git C:\Users\dev\repos\pulumi-structurizr-workshop
-cd C:\Users\dev\repos\pulumi-structurizr-workshop\scripts
+$username = "dev." + $env:computername
+
+$repoFolder = "C:\Users\" + $username + "\repos\pulumi-structurizr-workshop"
+
+& 'C:\Program Files\Git\cmd\git' clone https://github.com/ChristianEder/pulumi-structurizr-workshop.git $repoFolder
+cd $repoFolder
+cd scripts
 .\install-pulumi.ps1
 .\install-dependencies.ps1
 .\install-vs-code-extensions.ps1
 cd ..
 
+
 $WScriptShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WScriptShell.CreateShortcut("C:\Users\dev\Desktop\Open Workshop Repository in VS Code.lnk")
-$Shortcut.TargetPath = "vscode://file/C:/Users/dev/repos/pulumi-structurizr-workshop"
+$Shortcut = $WScriptShell.CreateShortcut("C:\Users\" +  $username + "\Desktop\Open Workshop Repository in VS Code.lnk")
+$Shortcut.TargetPath = "vscode://file/C:/Users/" + $username + "/repos/pulumi-structurizr-workshop"
 $Shortcut.IconLocation = "C:\Program Files\Microsoft VS Code\Code.exe"
 $Shortcut.Save()
--- scripts/install-vs-code-extensions.ps1
+++ scripts/install-vs-code-extensions.ps1
@@ -1,4 +1,6 @@
-& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension yzhang.markdown-all-in-one --force --extensions-dir 'C:\Users\dev\.vscode\extensions'
-& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension jebbs.plantuml --force --extensions-dir 'C:\Users\dev\.vscode\extensions'
-& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension mauve.terraform --force --extensions-dir 'C:\Users\dev\.vscode\extensions'
-& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension ms-vscode.powershell --force --extensions-dir 'C:\Users\dev\.vscode\extensions'
+$username = "dev." + $env:computername
+$extensionDir ="C:\Users\" + $username + "\.vscode\extensions"
+& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension yzhang.markdown-all-in-one --force --extensions-dir $extensionDir
+& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension jebbs.plantuml --force --extensions-dir $extensionDir
+& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension mauve.terraform --force --extensions-dir $extensionDir
+& 'C:\Program Files\Microsoft VS Code\bin\code' --install-extension ms-vscode.powershell --force --extensions-dir $extensionDir
