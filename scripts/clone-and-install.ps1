1791099ad614a62949ab8a3bde0c3a1e1a2f0889
Christian Eder <christian.eder@zuehlke.com>
2019-10-16 11:45:27 +0200

    fix user dirs again

--- scripts/clone-and-install.ps1
+++ scripts/clone-and-install.ps1
@@ -10,13 +10,20 @@ cd scripts
 .\install-vs-code-extensions.ps1
 cd ..
 
-Write-Host "### Creating desktop shortcut to VS Code"
-$userDir = .\get-user-dir.ps1
+$userDir = .\scripts\get-user-dir.ps1
 
+Write-Host "### Creating desktop shortcut to VS Code"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($userDir + "\Desktop\Open Workshop Repository in VS Code.lnk")
 $Shortcut.TargetPath = "vscode://file/C:/Users/" + $username + "/repos/pulumi-structurizr-workshop"
 $Shortcut.IconLocation = "C:\Program Files\Microsoft VS Code\Code.exe"
 $Shortcut.Save()
-
 Write-Host "### Done Creating desktop shortcut to VS Code"
+
+Write-Host "### Creating desktop shortcut to install VS Code extensions"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($userDir + "\Desktop\Install VS Code extensions.lnk")
+$Shortcut.TargetPath = $repoFolder + "\scripts\install-vs-code-extensions.ps1"
+$Shortcut.IconLocation = "C:\Program Files\Microsoft VS Code\Code.exe"
+$Shortcut.Save()
+Write-Host "### Done Creating desktop shortcut to install VS Code extensions"
--- scripts/get-user-dir.ps1
+++ scripts/get-user-dir.ps1
@@ -1,22 +1,27 @@
-Write-Host "### Getting user dir for dev user"
+Write-Host "### Getting user dir for dev user" 
 Write-Host("### - Current user is " + $env:UserName)
 
 $userDir ="C:\Users\dev"
+$desktop = $userDir + "\Desktop"
 
-if(Test-Path $userDir)
+if(Test-Path $desktop)
 {
+    Write-Host("### - Using " + $userDir)
     $userDir
 }
 else
 {
-    $userDir ="C:\Users\dev" + $env:computername
+    $userDir ="C:\Users\dev" + $env:computernamev
+    $desktop = $userDir + "\Desktop"
 
-    if(Test-Path $userDir)
+    if(Test-Path $desktop)
     {
+        Write-Host("### - Using " + $userDir)
         $userDir
     }
     else
     {
+        Write-Host("### - No user dir found")
         "not found"
     }
 }
