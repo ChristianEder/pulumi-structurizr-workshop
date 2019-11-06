d1fc41803685a5398c998bc7f97b2cf59d501589
Christian Eder <christian.eder@zuehlke.com>
2019-11-06 14:52:56 +0100

    add lab readme for structurizr

--- examples/getting-started/structurizr/README.md
+++ examples/getting-started/structurizr/README.md
@@ -1,13 +1,54 @@
 # Getting started with structurizr
-- After installing the required dependencies...
-  - NodeJS (https://nodejs.org/en/)
 
-You can now go ahead and start designing your architecture by changing the code in index.ts
-You can visualize your architecture diagram by
-- Running
+## Prerequisites
+
+> If you are doing this as part of the workshop [labs](../../../labs.md), you should be fine by now, and can skip the Prerequisites section.
+
+Otherwise, make sure you have the following tools installed:
+
+- [NodeJS](https://nodejs.org/en/). You can verify this by running the following command on the command line:
+  > npm -v
+- VS Code
+- PlantUml
+- GraphViz
+
+The easiest way to install those is to use [chocolatey](https://chocolatey.org/install):
+> choco install nodejs.install vscode plantuml graphviz
+  
+Also, if you are not doing this lab on one of the provided development VMs, you will initially have to install npm dependencies, by running the following command within the same directory as this README:
+
+> npm i
+
+## Lab tasks
+
+### 01 - Verify that your setup works
+
+- Open the repository folder (`C:\repos\pulumi-structurizr-workshop`) in VS Code
+- Open a terminal
+- Try visualizing the current state of the provided architecture model. The steps to do this are descibed in the cheat sheet section below
+- Compare the generated diagrams with the source code provided in [index.ts](index.ts) within the same directory as this README, in order to get a feeling for what the code actually does.
+
+### 02 -  Customize the architecture model
+
+You can now go ahead and start designing your architecture by changing the code in [index.ts](index.ts).
+
+You can either do this free style e.g. by starting from scractch and designing the actual architecture of one of your own systems, or you can start extending the provided example architecture model with some additional containers, surrounding systems or users. 
+
+Ideas for extensions of the existing model:
+- Add a new container that represents an administrative site used to manage the shops inventory, then add a new person "Inventory manager" using that site
+- Add a new container in between the 'shopWebsite' and the 'paymentProviderIntegration' that represents a queue that the 'shopWebsite' uses to communicate with the 'paymentProviderIntegration'
+- Add another container view that only shows the containers involved in inventory management (using the containerView.addContainer function instead of containerView.addAllContainers)
+- Try to automatically create a container view with only specific containers (like in the step before), by applying tags to containers, and then adding only container with that tag to the view
+
+## Cheatsheet
+
+All of the commands shown below are intended to be run on a command line from the same directory as this README file.
+
+- How to verify that my code builds?
+  > npm run build
+- How to visualize the architecture model I created?
   > npm start
-- opening the newly generated file `out/architecture.puml` n VS Code
-- Showing the diagram in VS Code by
+  - open the newly generated file `out/architecture.puml` in VS Code
   - Press F1
   - Select `PlantUML: Preview Current Diagram`
   - Navigate throught the different sections of the `out/architecture.puml` file
--- examples/getting-started/structurizr/index.ts
+++ examples/getting-started/structurizr/index.ts
@@ -1,38 +1,71 @@
 import { Workspace, Location, InteractionStyle, StructurizrClient, PlantUMLWriter } from 'structurizr-typescript'
 import * as fs from 'fs';
 
+// A workspace is the home for your complete architecture model
 const workspace = new Workspace();
 workspace.name = 'Your workspace name';
 
+// A software system is the top level item of architectural elements you
+// can describe in an architecture model.
+// The `shop` created below is the system that we are modeling here
 const shop = workspace.model.addSoftwareSystem('Shop', 'Online shopping system')!;
 shop.location = Location.Internal;
 
+// We can add more than 1 system to the same architecture model.
+// In this case, we add another, external system that we use to model
+// external dependencies of our `shop` system
 const paymentProvider = workspace.model.addSoftwareSystem('Payment Provider', 'Online payment provider')!;
 paymentProvider.location = Location.External;
 
+// This models a dependency from one system to another
 shop.uses(paymentProvider, 'process customer payments');
 
+// Also, we can add people interacting with those systems and 
+// model their interactions
 const user = workspace.model.addPerson('Customer', 'uses the shop to buy stuff')!;
 user.uses(shop, 'buy stuff');
 user.uses(paymentProvider, 'pay for stuff');
 
+// By adding containers to a software system, we dive 1 layer
+// deeper into modeling our architecture. Containers are typically
+// `things that do / provide something at runtime`
 const shopWebsite = shop.addContainer('Website', 'Lets users buy stuff', 'Shopware hosted on Azure')!;
 const productDatabase = shop.addContainer('Database', 'stores available products and customer orders', 'SQL Database')!;
 const paymentProviderIntegration = shop.addContainer('Payment integration', 'Manages integration of the payment provider', 'Azure function calling out to PayPal')!;
+
+// We can model dependencies between containers, as well as from containers
+// to other systems
 shopWebsite.uses(productDatabase, 'Load product data, save customer orders');
 shopWebsite.uses(paymentProviderIntegration, 'fulfill payments');
 paymentProviderIntegration.uses(paymentProvider, 'fulfil payments');
 paymentProviderIntegration.uses(productDatabase, 'mark orders as paid');
 
+// Also, we can model which containers inside a system the people specifically interact with
 user.uses(shopWebsite, 'buy stuff');
 
+// At this point, we are done with modeling the architecture.
+// The code that follows, is describing the views that we want to
+// generate onto the model we described above
+
+// A system context view shows 1 or more systems - as the name suggests - in context
+// By adding nearest neighbours, we will automatically see all elements
+// that have a direct dependency (either way) with the shop system
+// This includes the `user` person and `paymentProvider` system
 const systemContext = workspace.views.createSystemContextView(shop, 'shop-context', 'The system context view for the shop');
 systemContext.addNearestNeighbours(shop);
 
+// A container view shows either some or all containers of a system
+// and their dependencies to one another
+// Again, we can add neighbouring systems, containers, people to add 
+// more context to the diagram
 const containerView = workspace.views.createContainerView(shop, 'shop-containers', 'Container view for the shop');
 containerView.addAllContainers();
 containerView.addNearestNeighbours(shop);
 
+// Now that we finished modeling the architecture and describing
+// the views onto the model, we can either export the model
+// to plant uml, or we can upload it to structurizr.com, a
+// hosted backend to store architecture models and render diagrams.
 const plantUmlExport = new Promise((resolve, reject) => {
     const plantUML = new PlantUMLWriter().toPlantUML(workspace);
     fs.writeFile('out\\architecure.puml', plantUML, e => {
